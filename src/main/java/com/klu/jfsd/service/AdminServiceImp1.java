b54b89084dfef6173190a2731037cc15f1ec045a
sree <gudurivenusree@gmail.com>
2024-11-08 22:48:46 +0530

    adding to github

--- src/main/java/com/klu/jfsd/service/AdminServiceImp1.java
+++ src/main/java/com/klu/jfsd/service/AdminServiceImp1.java
@@ -12,6 +12,7 @@ public class AdminServiceImp1 implements AdminService {
     @Autowired
     private AdminRepository adminrepository;
     
+    //C:\Users\gudur\Desktop\SEM_REL\3.1 SEM\TAT\HomestayApp
     @Override
     public Admin checkAdminLogin(String username, String password) {
         return adminrepository.checkAdminLogin(username, password);
